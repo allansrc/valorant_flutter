598144537707893a97ca10e3446f652076754851
allansrc <allanramos.teixeira@gmail.com>
2023-01-05 11:49:05 -0300

    first commit

---
+++ lib/app/modules/home/controller/home_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeController extends ChangeNotifier {
+  final counter = ValueNotifier<int>(0);
+  // linha acima transforma a variável counter do tipo inteiro
+  // em um listenable: ou seja, ela poderá ser "ouvida" por widgets
+  // dessa forma não será necessário usar o método setState(() {}) para
+  // a tela ser atualizada.
+
+  // Para ver exemplo de uma função de incremento/soma de inteiros
+  // abrir o arquivo main, o qual usa setState(() {}) para atualizar o estado
+  // da tela. Dica: esperimente remover o setState, e tentar usar o botão +
+  // depois faça hot restart.
+
+  void incrementCounter(int newValue) {
+    counter.value = newValue;
+    // linha acima atribui novo valor á variável counter
+    counter.notifyListeners();
+    // método acima atualiza o widget que usar o valor counter
+  }
+}
